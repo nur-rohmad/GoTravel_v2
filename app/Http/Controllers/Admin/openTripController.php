bf15a7473ebcacf05d19ec2eabdcb806af665b40
nur-rohmad <nurrohmad776@gmail.com>
2023-05-09 09:17:52 +0700

    update

--- app/Http/Controllers/Admin/openTripController.php
+++ app/Http/Controllers/Admin/openTripController.php
@@ -8,7 +8,7 @@ use App\Models\Wisata;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-class openTripController extends Controller
+class OpenTripController extends Controller
 {
     public function index(Request $request)
     {
